80e76d06a9b25e6a02b074c1fb1bb9f90cd13336
Emily Harber <16492325+thecodepixi@users.noreply.github.com>
2021-04-25 16:11:14 -0400

    Add addiitonal onDelimiter test

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,20 @@
 import Split from '../index';
 
-test('onDelimiter splits on whitespace by default', () => {
-  let str = 'hello from split-it';
-  let expected = ['hello', 'from', 'split-it'];
-  expect(Split.onDelimiter(str)).toEqual(expected);
+describe('onDelimiter', () => {
+  test('splits on whitespace by default', () => {
+    let str = 'hello from split-it';
+    let expected = ['hello', 'from', 'split-it'];
+    expect(Split.onDelimiter(str)).toEqual(expected);
+  });
+
+  test('splits on the given delimiter when one is passed', () => {
+    let str = 'hello.from.split-it';
+    let str2 = 'hello1from1split-it';
+    let str3 = 'hello,from,split-it';
+    let expected = ['hello', 'from', 'split-it'];
+
+    expect(Split.onDelimiter(str, '.')).toEqual(expected);
+    expect(Split.onDelimiter(str2, '1')).toEqual(expected);
+    expect(Split.onDelimiter(str3, ',')).toEqual(expected);
+  });
 });
