7180dcedaf1e3ee6e07bf60634db70338a7f61bc
Emily Harber <16492325+thecodepixi@users.noreply.github.com>
2021-04-25 17:18:10 -0400

    Add .it method and tests. Reorganize test file.

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const SPECIAL = ['[', '^', '$', '.', '|', '?', '*', '+', '(', ')'];
 
 const Split = {
   /**
+   * The base method of the split-it library.
+   * Splits a given string on all "classic" delimiters:
+   * commas, colons, tabs, and returns
+   *
+   * @param {string} str the string to split
+   */
+  it: (str) => {
+    return str.split(/,|:|\t|\r|\n/g);
+  },
+
+  /**
    * Accepts an optional delimiter argument.
    * Defaults to splitting on whitespace only
-   * @param str the string to split
-   * @param del the chosen delimiter on which to split the given string
+   * @param {string} str the string to split
+   * @param {string} del the chosen delimiter on which to split the given string
    */
   onDelimiter: (str, del = null) => {
     if (del) {
--- test/index.test.js
+++ test/index.test.js
@@ -1,31 +1,67 @@
 import Split from '../index';
 
-describe('onDelimiter', () => {
-  test('splits on whitespace by default', () => {
-    let str = 'hello from split-it';
-    let expected = ['hello', 'from', 'split-it'];
-    expect(Split.onDelimiter(str)).toEqual(expected);
+const expected = ['hello', 'from', 'split-it'];
+
+describe('Split', () => {
+  describe('.it', () => {
+    test('returns an array with an empty string when one is passed', () => {
+      expect(Split.it('')).toEqual(['']);
+    });
+
+    test('splits a given string on commas', () => {
+      let str = 'hello,from,split-it';
+
+      expect(Split.it(str)).toEqual(expected);
+    });
+
+    test('splits a given string on colons', () => {
+      let str = 'hello:from:split-it';
+
+      expect(Split.it(str)).toEqual(expected);
+    });
+
+    test('splits a given string on tab spaces', () => {
+      let str = 'hello\tfrom\tsplit-it';
+
+      expect(Split.it(str)).toEqual(expected);
+    });
+
+    test('splits a given string on line breaks and returns', () => {
+      let str = 'hello\nfrom\rsplit-it';
+
+      expect(Split.it(str)).toEqual(expected);
+    });
   });
 
-  test('splits on the given delimiter when one is passed', () => {
-    let str = 'hello.from.split-it';
-    let str2 = 'hello1from1split-it';
-    let str3 = 'hello,from,split-it';
-    let expected = ['hello', 'from', 'split-it'];
+  describe('.onDelimiter', () => {
+    test('returns an array with an empty string when one is passed', () => {
+      expect(Split.onDelimiter('')).toEqual(['']);
+    });
 
-    expect(Split.onDelimiter(str, '.')).toEqual(expected);
-    expect(Split.onDelimiter(str2, '1')).toEqual(expected);
-    expect(Split.onDelimiter(str3, ',')).toEqual(expected);
-  });
+    test('splits on whitespace by default', () => {
+      let str = 'hello from split-it';
 
-  test('accurately splits when the delimiter must be escaped', () => {
-    let str = 'hello|from|split-it';
-    let str2 = 'hello?from?split-it';
-    let str3 = 'hello*from*split-it';
-    let expected = ['hello', 'from', 'split-it'];
+      expect(Split.onDelimiter(str)).toEqual(expected);
+    });
 
-    expect(Split.onDelimiter(str, '|')).toEqual(expected);
-    expect(Split.onDelimiter(str2, '?')).toEqual(expected);
-    expect(Split.onDelimiter(str3, '*')).toEqual(expected);
+    test('splits on the given delimiter when one is passed', () => {
+      let str = 'hello.from.split-it';
+      let str2 = 'hello1from1split-it';
+      let str3 = 'hello,from,split-it';
+
+      expect(Split.onDelimiter(str, '.')).toEqual(expected);
+      expect(Split.onDelimiter(str2, '1')).toEqual(expected);
+      expect(Split.onDelimiter(str3, ',')).toEqual(expected);
+    });
+
+    test('accurately splits when the delimiter must be escaped', () => {
+      let str = 'hello|from|split-it';
+      let str2 = 'hello?from?split-it';
+      let str3 = 'hello*from*split-it';
+
+      expect(Split.onDelimiter(str, '|')).toEqual(expected);
+      expect(Split.onDelimiter(str2, '?')).toEqual(expected);
+      expect(Split.onDelimiter(str3, '*')).toEqual(expected);
+    });
   });
 });
